236ea9304597e71a8235bc466c725175f059fb6a
Lucas Gallo <luka.sktt1@gmail.com>
2024-04-17 13:23:05 -0300

    Login form
    Se agrego el form de login con usuario, contraseña y recordar datos

---
+++ WinForm/Actividad2/frmUsuario.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Actividad2
+{
+    partial class frmUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Rdatos = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.User = new System.Windows.Forms.TextBox();
+            this.lbUser = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Rdatos
+            // 
+            this.Rdatos.AutoSize = true;
+            this.Rdatos.Location = new System.Drawing.Point(335, 230);
+            this.Rdatos.Name = "Rdatos";
+            this.Rdatos.Size = new System.Drawing.Size(99, 17);
+            this.Rdatos.TabIndex = 0;
+            this.Rdatos.Text = "Recordar datos\r\n";
+            this.Rdatos.UseVisualStyleBackColor = true;
+            this.Rdatos.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged_1);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(296, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "INGRESAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // User
+            // 
+            this.User.Location = new System.Drawing.Point(296, 98);
+            this.User.Name = "User";
+            this.User.Size = new System.Drawing.Size(182, 20);
+            this.User.TabIndex = 2;
+            this.User.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lbUser
+            // 
+            this.lbUser.AutoSize = true;
+            this.lbUser.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUser.Location = new System.Drawing.Point(353, 56);
+            this.lbUser.Name = "lbUser";
+            this.lbUser.Size = new System.Drawing.Size(67, 19);
+            this.lbUser.TabIndex = 3;
+            this.lbUser.Text = "Usuario";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(339, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Contraseña";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPassword.Location = new System.Drawing.Point(296, 189);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(182, 22);
+            this.tbPassword.TabIndex = 5;
+            this.tbPassword.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // frmUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbUser);
+            this.Controls.Add(this.User);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Rdatos);
+            this.Name = "frmUsuario";
+            this.Text = "frmUsuario";
+            this.Load += new System.EventHandler(this.frmUsuario_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox Rdatos;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox User;
+        private System.Windows.Forms.Label lbUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPassword;
+    }
+}
