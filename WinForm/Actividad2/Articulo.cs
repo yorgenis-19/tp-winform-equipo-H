7c67162839c17906779169632f794a6c5b28fa30
Yorgenis <91138262+yorgenis-19@users.noreply.github.com>
2024-04-17 13:33:12 -0300

    Clase articulo
    Se creo la clase articulo para la pantalla de cargar un articulo

---
+++ WinForm/Actividad2/Articulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Actividad2
+{
+    internal class Articulo
+    {
+        public int id { get; set; }
+        public string codigo { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public string marca { get; set; }
+        public string categoria { get; set; }
+        public decimal precio { get; set; }
+    }
+}
