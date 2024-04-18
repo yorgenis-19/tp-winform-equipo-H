f98d4ef290dfd5e09114ba8e87b25fd9fa097cbf
Yorgenis <91138262+yorgenis-19@users.noreply.github.com>
2024-04-18 12:30:59 -0300

    Ventana agregar marca
    Se realizo la ventana agregar marca

---
+++ WinForm/Actividad2/AgregarMarca.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Actividad2
+{
+    partial class AgregarMarca
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblNobre = new System.Windows.Forms.Label();
+            this.textID = new System.Windows.Forms.TextBox();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(236, 107);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(20, 16);
+            this.lblID.TabIndex = 0;
+            this.lblID.Text = "ID";
+            // 
+            // lblNobre
+            // 
+            this.lblNobre.AutoSize = true;
+            this.lblNobre.Location = new System.Drawing.Point(236, 146);
+            this.lblNobre.Name = "lblNobre";
+            this.lblNobre.Size = new System.Drawing.Size(56, 16);
+            this.lblNobre.TabIndex = 1;
+            this.lblNobre.Text = "Nombre";
+            // 
+            // textID
+            // 
+            this.textID.Location = new System.Drawing.Point(341, 107);
+            this.textID.Name = "textID";
+            this.textID.Size = new System.Drawing.Size(137, 22);
+            this.textID.TabIndex = 2;
+            // 
+            // textNombre
+            // 
+            this.textNombre.Location = new System.Drawing.Point(341, 146);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(137, 22);
+            this.textNombre.TabIndex = 3;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(204, 252);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(147, 53);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(403, 252);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(147, 53);
+            this.btnCancelar.TabIndex = 5;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // AgregarMarca
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.textID);
+            this.Controls.Add(this.lblNobre);
+            this.Controls.Add(this.lblID);
+            this.Name = "AgregarMarca";
+            this.Text = "AgregarMarca";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblNobre;
+        private System.Windows.Forms.TextBox textID;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
