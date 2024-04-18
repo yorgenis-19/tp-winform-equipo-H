1d8a4ee74fc8b2e716705fb7b7eb896a3800da2e
Yorgenis <91138262+yorgenis-19@users.noreply.github.com>
2024-04-18 09:45:15 -0300

    Clase imagen
    se creo la clase imagen

---
+++ WinForm/Actividad2/Imagen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad2
+{
+    internal class Imagen
+    {
+        public int id { get; set; }
+        public int idArticulo { get; set; }
+        public string imagenUrl { get; set; }
+    }
+}
