e903273612d72e31e8731b6385a74795153b6dd5
Yorgenis <91138262+yorgenis-19@users.noreply.github.com>
2024-04-21 12:41:21 -0300

    Clase acceso datos
    se realizo la clase acceso datos para la conexión a la base de datos y todos sus atributos

---
+++ WinForm/Actividad2/ArticuloDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Actividad2
+{
+    internal class ArticuloDB
+    {
+        public void agregar(Articulo nuevo)
+        {
+            
+        }
+    }
+}
