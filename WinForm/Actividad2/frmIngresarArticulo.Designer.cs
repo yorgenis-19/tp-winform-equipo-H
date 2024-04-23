a816a08a34bc05e5fbf92f6bb26be01c1cb10a38
Yorgenis <91138262+yorgenis-19@users.noreply.github.com>
2024-04-23 09:04:31 -0300

    agregar articulo a DB y validaciones
    se agrego el funcionamiento para cuando se cargue un nuevo articulo se guarde en la DB, tambien se agrego la validaciones de los datos ingresados para ese nuevo articulo

--- WinForm/Actividad2/frmIngresarArticulo.Designer.cs
+++ WinForm/Actividad2/frmIngresarArticulo.Designer.cs
@@ -35,181 +35,175 @@
             this.lblPrecio = new System.Windows.Forms.Label();
             this.lblIDCategoria = new System.Windows.Forms.Label();
             this.lblIDMarca = new System.Windows.Forms.Label();
-            this.txtCode = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.cmbBxIDMarca = new System.Windows.Forms.ComboBox();
-            this.cboCategoria = new System.Windows.Forms.ComboBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.cmbMarca = new System.Windows.Forms.ComboBox();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
             this.btnAgregar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
+            this.numPrecio = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numPrecio)).BeginInit();
             this.SuspendLayout();
             // 
             // lalID
             // 
             this.lalID.AutoSize = true;
-            this.lalID.Location = new System.Drawing.Point(160, 46);
-            this.lalID.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lalID.Location = new System.Drawing.Point(213, 57);
             this.lalID.Name = "lalID";
-            this.lalID.Size = new System.Drawing.Size(18, 13);
+            this.lalID.Size = new System.Drawing.Size(20, 16);
             this.lalID.TabIndex = 0;
             this.lalID.Text = "ID";
             // 
             // lblDescripcion
             // 
             this.lblDescripcion.AutoSize = true;
-            this.lblDescripcion.Location = new System.Drawing.Point(117, 169);
-            this.lblDescripcion.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDescripcion.Location = new System.Drawing.Point(156, 208);
             this.lblDescripcion.Name = "lblDescripcion";
-            this.lblDescripcion.Size = new System.Drawing.Size(63, 13);
+            this.lblDescripcion.Size = new System.Drawing.Size(79, 16);
             this.lblDescripcion.TabIndex = 1;
             this.lblDescripcion.Text = "Descripcion";
             // 
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(134, 127);
-            this.lblNombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNombre.Location = new System.Drawing.Point(179, 156);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.Size = new System.Drawing.Size(56, 16);
             this.lblNombre.TabIndex = 2;
             this.lblNombre.Text = "Nombre";
             // 
             // lblCodigo
             // 
             this.lblCodigo.AutoSize = true;
-            this.lblCodigo.Location = new System.Drawing.Point(138, 84);
-            this.lblCodigo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCodigo.Location = new System.Drawing.Point(184, 103);
             this.lblCodigo.Name = "lblCodigo";
-            this.lblCodigo.Size = new System.Drawing.Size(40, 13);
+            this.lblCodigo.Size = new System.Drawing.Size(51, 16);
             this.lblCodigo.TabIndex = 3;
             this.lblCodigo.Text = "Codigo";
             // 
             // lblPrecio
             // 
             this.lblPrecio.AutoSize = true;
-            this.lblPrecio.Location = new System.Drawing.Point(142, 306);
-            this.lblPrecio.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPrecio.Location = new System.Drawing.Point(189, 377);
             this.lblPrecio.Name = "lblPrecio";
-            this.lblPrecio.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecio.Size = new System.Drawing.Size(46, 16);
             this.lblPrecio.TabIndex = 4;
             this.lblPrecio.Text = "Precio";
             // 
             // lblIDCategoria
             // 
             this.lblIDCategoria.AutoSize = true;
-            this.lblIDCategoria.Location = new System.Drawing.Point(128, 256);
-            this.lblIDCategoria.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblIDCategoria.Location = new System.Drawing.Point(171, 315);
             this.lblIDCategoria.Name = "lblIDCategoria";
-            this.lblIDCategoria.Size = new System.Drawing.Size(52, 13);
+            this.lblIDCategoria.Size = new System.Drawing.Size(66, 16);
             this.lblIDCategoria.TabIndex = 5;
             this.lblIDCategoria.Text = "Categoria";
             // 
             // lblIDMarca
             // 
             this.lblIDMarca.AutoSize = true;
-            this.lblIDMarca.Location = new System.Drawing.Point(141, 213);
-            this.lblIDMarca.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblIDMarca.Location = new System.Drawing.Point(188, 262);
             this.lblIDMarca.Name = "lblIDMarca";
-            this.lblIDMarca.Size = new System.Drawing.Size(37, 13);
+            this.lblIDMarca.Size = new System.Drawing.Size(45, 16);
             this.lblIDMarca.TabIndex = 6;
             this.lblIDMarca.Text = "Marca";
             // 
-            // txtCode
+            // txtCodigo
             // 
-            this.txtCode.Location = new System.Drawing.Point(199, 81);
-            this.txtCode.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.txtCode.Name = "txtCode";
-            this.txtCode.Size = new System.Drawing.Size(137, 20);
-            this.txtCode.TabIndex = 1;
+            this.txtCodigo.Location = new System.Drawing.Point(265, 100);
+            this.txtCodigo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(181, 22);
+            this.txtCodigo.TabIndex = 1;
             // 
-            // textBox2
+            // txtID
             // 
-            this.textBox2.Location = new System.Drawing.Point(199, 43);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(137, 20);
-            this.textBox2.TabIndex = 0;
+            this.txtID.Location = new System.Drawing.Point(265, 53);
+            this.txtID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(181, 22);
+            this.txtID.TabIndex = 0;
             // 
-            // textBox3
+            // txtNombre
             // 
-            this.textBox3.Location = new System.Drawing.Point(199, 124);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(137, 20);
-            this.textBox3.TabIndex = 2;
+            this.txtNombre.Location = new System.Drawing.Point(265, 153);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(181, 22);
+            this.txtNombre.TabIndex = 2;
             // 
-            // textBox4
+            // txtDescripcion
             // 
-            this.textBox4.Location = new System.Drawing.Point(199, 306);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(137, 20);
-            this.textBox4.TabIndex = 6;
+            this.txtDescripcion.Location = new System.Drawing.Point(265, 204);
+            this.txtDescripcion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(181, 22);
+            this.txtDescripcion.TabIndex = 3;
             // 
-            // textBox7
+            // cmbMarca
             // 
-            this.textBox7.Location = new System.Drawing.Point(199, 166);
-            this.textBox7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(137, 20);
-            this.textBox7.TabIndex = 3;
+            this.cmbMarca.FormattingEnabled = true;
+            this.cmbMarca.Location = new System.Drawing.Point(265, 258);
+            this.cmbMarca.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmbMarca.Name = "cmbMarca";
+            this.cmbMarca.Size = new System.Drawing.Size(181, 24);
+            this.cmbMarca.TabIndex = 4;
             // 
-            // cmbBxIDMarca
+            // cmbCategoria
             // 
-            this.cmbBxIDMarca.FormattingEnabled = true;
-            this.cmbBxIDMarca.Location = new System.Drawing.Point(199, 210);
-            this.cmbBxIDMarca.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.cmbBxIDMarca.Name = "cmbBxIDMarca";
-            this.cmbBxIDMarca.Size = new System.Drawing.Size(137, 21);
-            this.cmbBxIDMarca.TabIndex = 4;
-            // 
-            // cboCategoria
-            // 
-            this.cboCategoria.FormattingEnabled = true;
-            this.cboCategoria.Location = new System.Drawing.Point(199, 256);
-            this.cboCategoria.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.cboCategoria.Name = "cboCategoria";
-            this.cboCategoria.Size = new System.Drawing.Size(137, 21);
-            this.cboCategoria.TabIndex = 5;
-            this.cboCategoria.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Location = new System.Drawing.Point(265, 315);
+            this.cmbCategoria.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(181, 24);
+            this.cmbCategoria.TabIndex = 5;
+            this.cmbCategoria.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(395, 90);
-            this.btnAgregar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAgregar.Location = new System.Drawing.Point(527, 111);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(146, 52);
+            this.btnAgregar.Size = new System.Drawing.Size(195, 64);
             this.btnAgregar.TabIndex = 7;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(395, 193);
-            this.btnCancelar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCancelar.Location = new System.Drawing.Point(527, 238);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(146, 52);
+            this.btnCancelar.Size = new System.Drawing.Size(195, 64);
             this.btnCancelar.TabIndex = 8;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
+            // numPrecio
+            // 
+            this.numPrecio.Location = new System.Drawing.Point(265, 377);
+            this.numPrecio.Name = "numPrecio";
+            this.numPrecio.Size = new System.Drawing.Size(181, 22);
+            this.numPrecio.TabIndex = 9;
+            // 
             // frmIngresarArticulo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(669, 366);
+            this.ClientSize = new System.Drawing.Size(892, 450);
+            this.Controls.Add(this.numPrecio);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnAgregar);
-            this.Controls.Add(this.cboCategoria);
-            this.Controls.Add(this.cmbBxIDMarca);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.cmbCategoria);
+            this.Controls.Add(this.cmbMarca);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtCodigo);
             this.Controls.Add(this.lblIDMarca);
             this.Controls.Add(this.lblIDCategoria);
             this.Controls.Add(this.lblPrecio);
@@ -217,10 +211,11 @@
             this.Controls.Add(this.lblNombre);
             this.Controls.Add(this.lblDescripcion);
             this.Controls.Add(this.lalID);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmIngresarArticulo";
             this.Text = "IngresarArticulo";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numPrecio)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -235,15 +230,15 @@
         private System.Windows.Forms.Label lblPrecio;
         private System.Windows.Forms.Label lblIDCategoria;
         private System.Windows.Forms.Label lblIDMarca;
-        private System.Windows.Forms.TextBox txtCode;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.ComboBox cmbBxIDMarca;
-        private System.Windows.Forms.ComboBox cboCategoria;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.ComboBox cmbMarca;
+        private System.Windows.Forms.ComboBox cmbCategoria;
         private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.NumericUpDown numPrecio;
     }
 }
 
--- WinForm/Actividad2/frmIngresarArticulo.cs
+++ WinForm/Actividad2/frmIngresarArticulo.cs
@@ -20,22 +20,127 @@ namespace Actividad2
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
             
-            string categoria = cboCategoria.SelectedItem.ToString();
+            string categoria = cmbCategoria.SelectedItem.ToString();
 
         }
 
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            cboCategoria.Items.Add("Articulos de limpieza");
-            cboCategoria.Items.Add("Articulos escolares");
-            cboCategoria.Items.Add("Articulos de cocina");
-            cboCategoria.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbCategoria.Items.Add("Articulos de limpieza");
+            cmbCategoria.Items.Add("Articulos escolares");
+            cmbCategoria.Items.Add("Articulos de cocina");
+            cmbCategoria.DropDownStyle = ComboBoxStyle.DropDownList;
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private bool validateFields()
+        {
+            if (txtCodigo.Text.Length == 0)
+            {
+                MessageBox.Show("Codigo sin llenar");
+                return false;
+            }
+            if (txtNombre.Text.Length == 0)
+            {
+                MessageBox.Show("Nombre sin llenar");
+                return false;
+            }
+            if (txtDescripcion.Text.Length == 0)
+            {
+                MessageBox.Show("Descripcion sin llenar");
+                return false;
+            }
+            if (!ValidarMarca())
+            {
+                return false;
+            }
+            if (!ValidarCategoria())
+            {
+                return false;
+            }
+            if (yaExiste())
+            {
+                MessageBox.Show("Ya existe un Articulo con ese codigo");
+                return false;
+            }
+            return true;
+        }
+
+        private bool ValidarMarca()
+        {
+            MarcaDB marcaDB = new MarcaDB();
+            List<Marca> listaMarca = new List<Marca>();
+            listaMarca = marcaDB.listar();
+
+            foreach (var marca in listaMarca)
+            {
+                if (marca.Descripcion == cmbMarca.Text)
+                {
+                    return true;
+                }
+            }
+            MessageBox.Show("Marca no valida");
+            return false;
+        }
+
+        private bool ValidarCategoria()
+        {
+            CategoriaDB categoriaDB = new CategoriaDB();
+            List<Categoria> listaCategoria = new List<Categoria>();
+            listaCategoria = categoriaDB.listar();
+            foreach (var categoria in listaCategoria)
+            {
+                if (categoria.Descripcion == cmbCategoria.Text)
+                {
+                    return true;
+                }
+            }
+            MessageBox.Show("Categoria no valida");
+            return false;
+        }
+
+        public bool yaExiste()
+        {
+            ArticuloDB articuloDB = new ArticuloDB();
+            List<Articulo> articulos = new List<Articulo>();
+            articulos = articuloDB.listar();
+            foreach (var articulo in articulos)
+            {
+                if (txtCodigo.Text == articulo.codigo)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (!validateFields())
+            {
+                return;
+            }
+            //MarcaDB marcaDB = new MarcaDB();
+            //CategoriaDB categoriaDB = new CategoriaDB();
+
+            //ArticuloDB articuloDB = new ArticuloDB();
+            Articulo articulo = new Articulo();
+            articulo.codigo = txtCodigo.Text;
+            articulo.nombre = txtNombre.Text;
+            articulo.descripcion = txtDescripcion.Text;
+            articulo.Marca = new Marca();
+            articulo.Marca.Descripcion = cmbMarca.Text;
+            articulo.Marca.Id = marcaDB.obtener(articulo.marca.Descripcion);
+            articulo.Categoria = new Categoria();
+            articulo.Categoria.Descripcion = cmbCategoria.Text;
+            articulo.Categoria.Id = categoriaDB.obtener(articulo.categoria.Descripcion);
+            articulo.precio = numPrecio.Value;
+
+            //articuloDB.agregar(articulo);
         }
     }
 }
